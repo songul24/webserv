00d3d043b61f5a07b2eab04cae006c21bbdc30d9
mlshawqi <marriyash77@.com>
2026-04-15 15:24:25 +0100

    delete method

--- src/Delete_method.cpp
+++ src/Delete_method.cpp
@@ -1,19 +1,19 @@
 #include "../include/WebServer.hpp"
 
-
 std::string Delete_folder(const std::string& path)
 {
         if(access(path.c_str(), W_OK) != 0)
                 return (std::string("403 Forbidden"));
+        // DIR *opendir(path.c_str());
+
 }
 
-std::string Delete_mothod(const std::string& url, const std::string& root)
+std::string Delete_file(const std::string& path)
 {
-        if(url.empty() || url.find("..") != std::string::npos)
+        if(path.empty() || path.find("..") != std::string::npos)
                 return (std::string("404 Not found"));
         
         struct stat buf;
-        std::string path = root + url;
         if(stat(path.c_str(), &buf) != 0)
         {
                 if (S_ISDIR(buf.st_mode))
@@ -27,4 +27,13 @@ std::string Delete_mothod(const std::string& url, const std::string& root)
         if(std::remove(path.c_str()) != 0)
                 return (std::string("403 Forbidden"));
         return (std::string("204 No Content"));
+}
+
+
+std::string Delete_mothod(const std::string& url, const std::string& root)
+{
+        if(url.empty() || url.find("..") != std::string::npos)
+                return (std::string("404 Not found"));
+        
+        return Delete_file(url + root);
 }
